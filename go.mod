e19d5e7a25e605f1b1219fe4b2290909dcc5778b
Jiajian Yang <jiajian.yang@searchspring.com>
2023-03-03 12:37:57 -0500

    initial dump

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/yangjeep/synonym-playground
+
+go 1.19
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/sashabaranov/go-gpt3 v1.2.1
+)
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/joho/godotenv"
+	gogpt "github.com/sashabaranov/go-gpt3"
+)
+
+type Completion struct {
+	Text string `json:"text"`
+}
+
+type Choice struct {
+	Text string `json:"text"`
+}
+
+type OpenAIResponse struct {
+	Completions []Completion `json:"completions"`
+}
+
+func generateSynonyms(apiKey string, word string, numSynonyms int) ([]string, error) {
+	prompt := fmt.Sprintf("Generate %d synonyms for %s:", numSynonyms, word)
+	data := url.Values{
+		"prompt":      {prompt},
+		"max_tokens":  {"100"},
+		"temperature": {"0.5"},
+		"n":           {"1"},
+		"stop":        {""},
+	}
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", "https://api.openai.com/v1/engines/text-davinci-002/completions", strings.NewReader(data.Encode()))
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Authorization", "Bearer "+apiKey)
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, err
+	}
+	var response OpenAIResponse
+	if err := json.Unmarshal(body, &response); err != nil {
+		return nil, err
+	}
+	message := response.Completions[0].Text
+
+	re := regexp.MustCompile(`"(.+)"`)
+	synonyms := re.FindAllString(message, numSynonyms)
+	return synonyms, nil
+}
+
+func main() {
+	godotenv.Load()
+	apiKey := os.Getenv("OPENAI_API_KEY")
+	if apiKey == "" {
+		log.Fatalln("Missing API KEY")
+	}
+
+	c := gogpt.NewClient(apiKey)
+	ctx := context.Background()
+
+	req := gogpt.CompletionRequest{
+		Model:       gogpt.GPT3Davinci,
+		MaxTokens:   100,
+		Temperature: 0.5,
+		Prompt:      "Generate 10 synonyms for swimsuit",
+		//Stream:    true,
+	}
+
+	resp, err := c.CreateCompletion(ctx, req)
+	if err != nil {
+		return
+	}
+	fmt.Println(resp.Choices[0].Text)
+}
